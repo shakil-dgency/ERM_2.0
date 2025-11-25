933cba1dda7438fa28b5a8a5d0da987d456ef618
shakil-dgency <shakil.dgency@gmail.com>
2025-11-25 14:10:35 +0600

    resolve deployment build error

--- src/app/terms-of-service/page.js
+++ src/app/terms-of-service/page.js
@@ -1,10 +1,6 @@
 import React from "react";
 
-async function page() {
-	const url = `${process.env.NEXT_PUBLIC_API_URL}/api/team`;
-
-	const { data } = await getData(url, "team page");
-
+function page() {
 	return <div>page</div>;
 }
 
